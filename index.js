431ff269263e3fec3c36ffaed8a52b3c3c0a40f7
gaillour <juangaillour.8@gmail.com>
2021-06-03 01:08:49 -0300

    adding files
    index.html index.js styles.css

---
+++ index.js
@@ -0,0 +1,29 @@
+values = [];
+
+function addRecord() {
+  var inp = document.getElementById('inputtext');
+  values.push(inp.value);
+  inp.value = "";
+  displayRecord();
+}
+
+function displayRecord() {
+  document.getElementById("jugadores").innerHTML = values.join(", ");
+}
+
+function choose(choices) {
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
+
+var retos = [
+  'tomar si o si por pete',
+  'escribirle lo que elijan los de mas a quien elijan los de mas',
+  'agregarle rey a todo lo que diga',
+  'pedirle a alguien de la escuela algo random que elijan los de mas',
+  'llamar a alguien y decirle que lo ama'
+]
+
+function darReto() {
+  document.getElementById('reto').innerHTML = choose(values) + ' tiene que ' + choose(retos);
+}
