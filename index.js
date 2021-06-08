baf3506d1e908c7b10716c196a4df0112fe6c12b
gaillour <juangaillour.8@gmail.com>
2021-06-07 21:47:19 -0300

    shuffle to random

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-values = [];
+jugadores_inicial = [];
 
 function addRecord() {
   var inp = document.getElementById('inputtext');
-  values.push(inp.value);
+  jugadores_inicial.push(inp.value);
   inp.value = "";
   displayRecord();
 }
 
 function displayRecord() {
-  document.getElementById("jugadores").innerHTML = values.join(", ");
+  document.getElementById("jugadores").innerHTML = jugadores_inicial.join(", ");
 }
 
 function choose(choices) {
@@ -17,11 +17,11 @@ function choose(choices) {
 }
 
 
-let retos = [
+var retos_inicial = [
   'toma si o si por pete',
   'tiene que escribirle lo que elijan los de mas a quien elijan los de mas',
-  'tiene que agregarle rey a todo lo que diga',
-  'tiene que llamar a alguien y decirle que la ama',
+  'agregale rey a todo lo que digas por una ronda',
+  'llama a la persona que elijan los demas y decile que la amas',
   'tiene que darle un beso a la persona de su izquierda',
   'tiene que darle un beso a la persona de su derecha',
   'elegi a alguien para que sea tu dueño, y tenes que hacer todo lo que diga',
@@ -45,6 +45,36 @@ let retos = [
   'los demas deciden el reto'
 ]
 
-function darReto() {
-  document.getElementById('reto').innerHTML = choose(values) +' '+ choose(retos);
+
+function shuffle(array) {
+    var m = array.length, t, i;
+  
+    // While there remain elements to shuffle…
+    while (m) {
+  
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+  
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+  
+    return array;
+}
+
+var retos = shuffle(retos_inicial)
+console.log(retos_inicial)
+
+
+
+var i=0;
+function darReto(){
+    document.getElementById("reto").innerHTML = choose(jugadores_inicial) + " "+ retos[i];
+    i++;
+    if(retos[i + 1] === undefined){
+        i=0;
+        shuffle(retos);
+    }
 }
